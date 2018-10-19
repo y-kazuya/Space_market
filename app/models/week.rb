520cdd14ae51660825d047e8810255778a4c9ecd
y-kazuya <y.kazuya.0124@au.com>
2018-10-19 15:21:28 +0900

    Modify

--- app/models/week.rb
+++ app/models/week.rb
@@ -2,6 +2,6 @@ class Week < ApplicationRecord
   belongs_to :plan
 
   validates :name, presence: true #各曜日が入る
-  validates :start, presence: true, numericality: { only_integer: true, less_than_or_equal_to: 36 }, length: { maximum: 7 }
-  validates :end, presence: true, numericality: { only_integer: true, less_than_or_equal_to: 36 }, length: { maximum: 7 }
+  validates :start, presence: true, numericality: { only_integer: true, less_than_or_equal_to: 36 }
+  validates :end, presence: true, numericality: { only_integer: true, less_than_or_equal_to: 36 }
 end
