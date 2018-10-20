43ab2712c8259de4fd19bfc4a0c4fa0d3866399c
y-kazuya <y.kazuya.0124@au.com>
2018-10-20 22:52:32 +0900

    Merge branch 'master' into create_room&speace
    # Conflicts:
    #	app/views/test/first.html.haml
    #	config/routes.rb
    #	db/schema.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root "pages#top"
+  devise_for :users
 
   resource :pages do
     collection do
--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20181020041333) do
 
   create_table "basic_info_usages", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
@@ -97,6 +98,9 @@ ActiveRecord::Schema.define(version: 20181020041333) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+=======
+ActiveRecord::Schema.define(version: 20181020083148) do
+>>>>>>> master
 
   create_table "tests", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name"
@@ -104,6 +108,7 @@ ActiveRecord::Schema.define(version: 20181020041333) do
     t.datetime "updated_at", null: false
   end
 
+<<<<<<< HEAD
   create_table "usages", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name",       null: false
     t.datetime "created_at", null: false
@@ -130,4 +135,26 @@ ActiveRecord::Schema.define(version: 20181020041333) do
   add_foreign_key "rooms", "spaces"
   add_foreign_key "space_infos", "spaces"
   add_foreign_key "weeks", "plans"
+=======
+  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "last_name",                                            null: false
+    t.string   "first_name",                                           null: false
+    t.string   "email",                                                null: false
+    t.string   "encrypted_password",                                   null: false
+    t.string   "company"
+    t.string   "avatar"
+    t.text     "profile",                limit: 65535
+    t.string   "URL"
+    t.integer  "timezone"
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at",                                           null: false
+    t.datetime "updated_at",                                           null: false
+    t.boolean  "admin",                                default: false
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+  end
+
+>>>>>>> master
 end
