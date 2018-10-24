c75c00dd56189797c235012a7b2e0445b19092c3
Owner <>
2018-10-24 14:19:06 +0900

    create header and footer

--- app/assets/javascripts/top-page.js
+++ app/assets/javascripts/top-page.js
@@ -1,8 +1,13 @@
 $(function(){
+  $(document).on('click', function(e) {
+
+  });
   $(".top-nav-down").on('click',function(e) {
+
     $(".top-nav-parent").css({
       'visibility': 'visible',
       'opacity': '1'
     });
   });
+   
 })
