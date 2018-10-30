62fc9510b1a1c1edcc0bafece54922460d020ca2
ayaa-machida <mamamamacchan@gmail.com>
2018-10-30 17:28:09 +0900

    ファイル追加

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,6 +1,5 @@
 class Favorite < ApplicationRecord
   belongs_to :room
   belongs_to :favorite_list
-
   validates :room_id, uniqueness: { scope: [:favorite_list_id]  }
 end
--- app/models/favorite_list.rb
+++ app/models/favorite_list.rb
@@ -2,6 +2,5 @@ class FavoriteList < ApplicationRecord
   belongs_to :user
   has_many :favorites
   has_many :rooms, through: :favorites
-
   validates :name, presence: true, uniqueness: { scope: [:user_id]  }
 end
