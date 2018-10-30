fe3081d4445a068991fb59b34cc6491f06ec6714
ayaa-machida <mamamamacchan@gmail.com>
2018-10-30 20:01:26 +0900

    修正

--- db/migrate/20181030104512_create_favorites.rb
+++ db/migrate/20181030104512_create_favorites.rb
@@ -1,7 +1,6 @@
 class CreateFavorites < ActiveRecord::Migration[5.0]
   def change
     create_table :favorites do |t|
-      create_table :favorites do |t|
       t.references :room,          foreign_key: true, null: false
       t.references :favorite_list, foreign_key: true, null: false
       t.timestamps
