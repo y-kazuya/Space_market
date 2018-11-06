3ebed6064efabc9aa0bb89bfdc003b1e515c33ba
hiroaki <hiroaki51@gmail.com>
2018-11-06 10:55:28 +0900

    header

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,8 @@ class PagesController < ApplicationController
   before_action :login?, only: [:host_entry]
 
   def top
-    @spaces = Space.public_spaces
+    # @spaces = Space.public_spaces
+    render "tmp/room_registration_main"
   end
 
   def host_entry
