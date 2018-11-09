99de3f984e7854d21363ed2fab0057918a69e1e6
y-kazuya <y.kazuya.0124@au.com>
2018-11-09 15:33:19 +0900

    Eat

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -14,29 +14,64 @@ class SearchController < ApplicationController
   def index
     state_name = params[:state][:name]
     state_id = SpaceInfo.states[state_name.to_s]
-    max_price = params[:max_price][:price]
-    max_capacity = params[:max_capacity][:capacity]
+
+    params[:max_price][:price] != "" ?  max_price = params[:max_price][:price].to_i : max_price = 1000000000000000000000000000
+    params[:max_capacity][:capacity] != "" ? max_capacity = params[:max_capacity][:capacity].to_i : max_capacity = 0
+    # max_price = params[:max_price][:price].to_i
+    # max_capacity = params[:max_capacity][:capacity].to_i
     # 検索結果格納
-    @results = []
+
+
+
     @rooms = []
     # 認証済であり、公開されている部屋を絞る。さらにエリアで絞り込む
     if params[:state][:name]
-      @rooms = Room.joins(space: [:space_info]).where("state = ?", "#{state_id}" ).distinct.where(activated: "certification").where(rooms: {public: true })
+      results = Room.joins(space: [:space_info]).where("state = ?", "#{state_id}" ).distinct.where(activated: "certification").where(rooms: {public: true })
+
     end
-    unless @rooms.length == 0
-       #さらに金額で絞り込む
-      @rooms.each do |room|
-        room.plans.each do |plan|
-          if plan.day_price <= max_price.to_i
-            #さらに人数で絞り込む
-            if room.basic_info.capacity >= max_capacity.to_i
-              @results << room
-              break
-            end
-          end
+
+
+    if results.length >= 1
+
+      @kane = []
+
+      results.each do |room|
+        room.public_plans.each do |plan|
+
+          @kane << room if plan.day_price <= max_price
+            break
         end
       end
+      results = @kane
     end
+
+
+    if results.length >= 1
+      @oo = []
+
+      results.each do |room|
+          @oo << room if room.basic_info.capacity >= max_capacity
+        end
+     results = @oo
+    end
+
+
+    @results = results
+
+    # if @rooms.length >= 1
+    #    #さらに金額で絞り込む
+    #   @rooms.each do |room|
+    #     room.public_plans.each do |plan|
+    #       if plan.day_price <= max_price
+    #         #さらに人数で絞り込む
+    #         if room.basic_info.capacity >= max_capacity
+    #           @results << room
+    #           break
+    #         end
+    #       end
+    #     end
+    #   end
+    # end
     render :index, layout: false
   end
 end
