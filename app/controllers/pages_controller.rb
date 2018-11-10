22e6118e9eebb63967c87255a0d32e09670e4b95
y-kazuya <y.kazuya.0124@au.com>
2018-11-10 14:51:02 +0900

    画面合わせ、space_info

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,7 +4,7 @@ class PagesController < ApplicationController
   def top
     # @spaces = Space.public_spaces
     @space_info = SpaceInfo.new
-    render "tmp/room_registration_main"
+
   end
 
   def host_entry
